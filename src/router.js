baa5f791a9b0152b74ad9911ca2a06810cbd89e8
Vitor221 <vitornascimento6650@gmail.com>
2023-05-10 10:51:42 -0300

    implementando componente Home e router children

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Site from '@/views/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
 
 const routes = [
   {
@@ -11,7 +13,11 @@ const routes = [
   },
   {
     path: '/home', //localhost:8080/home
-    component: Home
+    component: Home,
+    children: [
+      { path: 'vendas', component: Vendas }, //localhost:8080/home/vendas
+      { path: 'servicos', component: Servicos} //localhost:8080/home/servicos
+    ]
   },
   {
     path: '/login', //localhost:8080/login
